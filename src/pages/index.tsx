5fc5b7bf92b07361a866ebe6cb9ff07c70eb129d
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-26 00:26:24 -0300

    feat: ssr request :beers:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import { formatCurrency } from '../utils/format';
 import styles from './home.module.scss';
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+};
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +27,27 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />{' '}
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IZ1DMHK5AfQrwIMEhNryJ3a');
+
+  const product = {
+    priceId: price.id,
+    amount: formatCurrency(price.unit_amount),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
