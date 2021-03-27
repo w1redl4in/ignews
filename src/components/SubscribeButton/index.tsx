ba115494e31462a39dc951b0c2182a2ff29b103c
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-27 20:08:38 -0300

    feat: implementing subscribe by requesting our api route :boom:

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/client';
+import { useCallback } from 'react';
+import { api } from '../../services/axios';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 type SubscribeButtonProps = {
@@ -5,8 +9,33 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  const handleSubscribe = useCallback(async () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }, [session]);
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
