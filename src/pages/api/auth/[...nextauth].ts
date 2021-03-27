1d2ef41a6ce90218b6635f0d4e9313ef238f8f3d
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-27 19:20:02 -0300

    feat: saving user.email to faunadb in login but checking if exists :sparkles:

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,26 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), {
+              data: { email },
+            }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
