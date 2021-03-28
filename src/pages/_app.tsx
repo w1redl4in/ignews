5ac9d38a508d6f553d091b091e3f22a4332605a8
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-28 00:09:21 -0300

    refactor: adding title tag to app instead :recycle:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Header } from '../components/Header';
 import { Provider as NextAuthProvider } from 'next-auth/client';
 
@@ -7,6 +8,9 @@ import '../styles/global.scss';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
+      <Head>
+        <title>ig.news</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </NextAuthProvider>
