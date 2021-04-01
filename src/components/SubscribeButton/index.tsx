d421b82e739d0c19b18ef8a25cbd8ed47502fdd1
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-01 16:30:37 -0300

    feat: redirecting user to /posts if he has a subscription :sparkles:

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 import { api } from '../../services/axios';
 import { getStripeJs } from '../../services/stripe-js';
@@ -9,11 +10,17 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const [session] = useSession();
+  const [session]: any = useSession();
+  const router = useRouter();
 
   const handleSubscribe = useCallback(async () => {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
