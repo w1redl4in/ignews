e9ed3dd7dceb92411e5e7acb3349455d41ae9bbc
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-01 16:09:14 -0300

    refactor: cleaning unused code :recycle:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,8 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { ActiveLink } from '../ActiveLink';
 
 export const Header: React.FC = () => {
-  const { asPath } = useRouter();
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
